9262f5f79cb3d99344deb13ab118a10949f4eba6
mflavin <mflavin@hawk.iit.edu>
2018-11-28 00:39:11 -0600

    Passed data from what event was clicked on. Added ids. Removed unneeded code.

--- site.js
+++ site.js
@@ -1,11 +1,5 @@
 const homepage = {
   template: $("#homepage").html(),
-  data()
-  {
-      return {
-          itemQuantity: 0
-      };
-  },
   methods: {
     searching: function() {
       router.push({ path: 'search', component: search })
@@ -20,6 +14,7 @@ const search = {
           searchString: $(".siteSearch").val(),
           article: [
             {
+              "id" : 0,
               "rating": '<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i>',
               "dollar": "$$$",
               "title": "Maria's",
@@ -27,6 +22,7 @@ const search = {
               "image": "http://www.community-bar.com/wp-content/uploads/2010/11/cropped-marias_masthead_optimized11.jpg"
             },
             {
+              "id" : 1,
               "rating": '<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i>',
               "dollar": "$$",
               "title": "Adler Planetarium",
@@ -34,6 +30,7 @@ const search = {
               "image": "https://upload.wikimedia.org/wikipedia/commons/7/7e/Adler_external_1.jpg"
             },
             {
+              "id" : 2,
               "rating": '<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i>',
               "dollar": "$",
               "title": "Sears Tower",
@@ -65,6 +62,11 @@ const search = {
   }
 };
 
+const zoomedInfo = {
+  props: ['id', 'image', 'rating', 'dollar', 'title'],
+  template: $("#zoomedInfo").html(),
+}
+
 const routes = [
     {
         path: "/",
@@ -74,6 +76,12 @@ const routes = [
     {
         path: "/search",
         component: search,
+        props: true
+    },
+    {
+        path: "/zoomedInfo",
+        name: 'zoomedInfo',
+        component: zoomedInfo,
         props: true
     }
 ];
@@ -86,74 +94,3 @@ const app = new Vue({
   el: '#app',
   router
 });
-
-// var demo = new Vue({
-//   el: '#main',
-//   // router,
-//   data: {
-//     searchString: "",
-//     articles: [{
-//         "rating": '<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i>',
-//         "dollar": "$$$",
-//         "title": "Maria's",
-//         "url": "http://www.community-bar.com/",
-//         "image": "http://www.community-bar.com/wp-content/uploads/2010/11/cropped-marias_masthead_optimized11.jpg"
-//       },
-//       {
-//         "rating": '<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i>',
-//         "dollar": "$$",
-//         "title": "Museum of Science and Industry",
-//         "url": "https://www.msichicago.org/",
-//         "image": "https://www.radiomuseum.org/museum/usa/museum-of-science-and-industry-msi-chicago/images/us_museum_of_science_and_industry_chicago_building_side.jpg"
-//       },
-//       {
-//         "rating": '<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i>',
-//         "dollar": "$$",
-//         "title": "Adler Planetarium",
-//         "url": "https://www.adlerplanetarium.org/",
-//         "image": "https://upload.wikimedia.org/wikipedia/commons/7/7e/Adler_external_1.jpg"
-//       },
-//       {
-//         "rating": '<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i>',
-//         "dollar": "$",
-//         "title": "Sears Tower",
-//         "url": "http://www.thesearstower.com/",
-//         "image": "http://www.trbimg.com/img-58d2f51a/turbine/ct-willis-tower-renovation-0323-biz-jpg-20170322/750/750x422"
-//       },
-//       {
-//         "rating": '<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i>',
-//         "dollar": "$$$",
-//         "title": "Chicago Art Museum",
-//         "url": "https://www.artdesignchicago.org/",
-//         "image": "https://media.timeout.com/images/100892139/1024/576/image.jpg"
-//       },
-//       {
-//         "rating": '<i class="fas fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i>',
-//         "dollar": "$$$$",
-//         "title": "Shedd Aquarium",
-//         "url": "https://www.sheddaquarium.org/",
-//         "image": "http://www.trbimg.com/img-5887ba95/turbine/ct-shedd-is-free-for-veterans-on-veterans-day--001/1150/1150x647"
-//       }
-//     ]
-//   },
-//   computed: {
-//     filteredArticles: function() {
-//       var articles_array = this.articles,
-//         searchString = this.searchString;
-//
-//       if (!searchString) {
-//         return articles_array;
-//       }
-//
-//       searchString = searchString.trim().toLowerCase();
-//
-//       articles_array = articles_array.filter(function(item) {
-//         if (item.title.toLowerCase().indexOf(searchString) !== -1) {
-//           return item;
-//         }
-//       });
-//
-//       return articles_array;
-//     }
-//   }
-// });
